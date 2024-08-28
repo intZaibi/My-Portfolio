f8c55b6b460f9fdea71c206c3076d60f802215be
IntZaibi <154490572+intZaibi@users.noreply.github.com>
2024-08-28 13:19:12 +0500

    Delete backend directory

--- backend/db.js
+++
@@ -1,8 +0,0 @@
-import mysql from 'mysql2/promise.js'
-export default mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '1234',
-    database: 'portfoliodb',
-    port: '3307'
-})
