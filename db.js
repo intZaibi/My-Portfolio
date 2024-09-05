dfdbe926fca7321e5f1fac604cbb1e6b1e8b79c3
IntZaibi <154490572+intZaibi@users.noreply.github.com>
2024-09-05 07:56:17 +0500

    Update db.js

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ import mysql from 'mysql2/promise.js'
 export default mysql.createPool({
     host: 'sql12.freesqldatabase.com',
     user: 'sql12728250',
-    password: 'qadri...',
+    password: '',
     database: 'sql12728250',
     port: '3306'
 })
